a8d8cc10c5a76b8ebc2189d9996b1a20d4a22930
timg777 <timg777@yandex.ru>
2025-10-02 18:49:39 +0500

    refactor(sprint_20): StoryContentView refactored and simplified

--- TravelSchedule/Presentation/Stories/StoryContentView.swift
+++ TravelSchedule/Presentation/Stories/StoryContentView.swift
@@ -1,76 +1,48 @@
 import SwiftUI
 
 struct StoryContentView: View {
+	// --- internal constants ---
+	let story: StoryModel
+	let showProgress: Bool
+	let currentStoryPartIndex: Int
+	let progress: CGFloat
 	
-	@StateObject private var dragGestureModel = DragGestureObject()
-	@Environment(StoriesViewModel.self) private var viewModel
-	
+	// --- body ---
 	@ViewBuilder
 	var body: some View {
-		let size = viewModel.screenSize
-		
-		if let currentStory = viewModel.currentStory {
-			let storyParts = currentStory.storyParts
-			let currentStoryIndex = viewModel.currentStoryIndex
-			let currentPartIndex = viewModel.currentStoryPartIndex
-			
-			if let storyPart = storyParts[safe: currentPartIndex] {
-				
-				ZStack {
-					StoryPartView(storyPart: storyPart)
-						.frame(width: size.width)
-						.overlay(alignment: .top) {
-							StoryTopLevelElementsView(
-								storyID: currentStory.id,
-								storyPartsCount: currentStory.storyParts.count
-							)
-						}
-						.offset(y: dragGestureModel.verticalDragValue)
-						.scaleEffect(1 - (dragGestureModel.verticalDragValue / (size.height)))
-						.animation(
-							dragGestureModel.verticalDragValue == 0 ?
-								.linear(duration: 0.15) : nil,
-							value: dragGestureModel.verticalDragValue
-						)
-						.animation(.default, value: currentPartIndex)
-						.animation(.default, value: currentStoryIndex)
+		if let storyPart = story.storyParts[safe: currentStoryPartIndex] {
+			StoryPartView(storyPart: storyPart)
+				.overlay(alignment: .topLeading) {
+					VStack(alignment: .trailing, spacing: 16) {
+						progressView
+							.frame(height: .progressBarHeight)
+							.padding(.horizontal, 8)
+						
+						CloseButtonView()
+						Spacer()
+					}
+					.safeAreaPadding(.top)
+					.padding(.top, 16)
 				}
-				.frame(
-					width: size.width,
-					height: size.height,
-					alignment: .center
-				)
-				.onAppear(perform: setupDragModelBindings)
-				.gesture(dragGestureModel.makeStoryGesture())
-			}
 		}
 	}
-}
-
-
-// MARK: - StoryView Extensions
-// MARK: Private Methods
-private extension StoryContentView {
-	func setupDragModelBindings() {
-		dragGestureModel.dismiss = { [weak viewModel] in
-			viewModel?.performDismissView()
-		}
-		dragGestureModel.invalidateTimer = { [weak viewModel] in
-			viewModel?.invalidateTimer()
-		}
-		dragGestureModel.performPage = { [weak viewModel] spec in
-			guard let viewModel else { return }
-			switch spec {
-			case .none:
-				break
-			case .next:
-				viewModel.goToNextPartOrStory()
-			case .prev:
-				viewModel.goToPrevPartOrStory()
+	
+	// --- private subviews ---
+	private var progressView: some View {
+		Group {
+			let storyPartsCount = story.storyParts.count
+			
+			if showProgress {
+				StoriesProgressView(
+					storyPartsCount: storyPartsCount,
+					progress: progress
+				)
+			} else {
+				ProgressBar(
+					storyPartsCount: storyPartsCount,
+					progress: 0
+				)
 			}
-		}
-		dragGestureModel.screenSize = { [viewModel] in
-			viewModel.screenSize
 		}
 	}
 }
