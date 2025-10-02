2af993e4ced458084ec625867ba61513c379fbbd
timg777 <timg777@yandex.ru>
2025-10-02 17:56:08 +0500

    refactor(sprint_20): StoriesManager fully erased

--- TravelSchedule/Managers/StoriesManager.swift
+++ TravelSchedule/Managers/StoriesManager.swift
@@ -14,17 +14,4 @@ final class StoriesManager {
     func apply(updatedStories: [StoryModel]) {
         self.stories = updatedStories
     }
-    
-	// MARK: - Refresh story parts on "isCheckedOut -> false" - if story fully checked
-    func resetStoryIfFullyViewed(at index: Int) {
-        guard stories.indices.contains(index) else { return }
-        let story = stories[index]
-        guard story.storyParts.allSatisfy(\.isCheckedOut) else { return }
-        let resetParts = story.storyParts.map { $0.invertedCheckedOutStatus(to: false) }
-        stories[index] = StoryModel(
-            id: story.id, // save story identifier
-            previewImageResource: story.previewImageResource,
-            storyParts: resetParts
-        )
-    }
 }
