393a3527577c73f1695cb541b360fc02bb361867
timg777 <timg777@yandex.ru>
2025-10-02 18:47:40 +0500

    feat(sprint_20): ProgressBar created and moved to file (allows controlling progress using custom progress bar)

--- TravelSchedule/Presentation/Stories/StoriesProgressView.swift
+++ TravelSchedule/Presentation/Stories/StoriesProgressView.swift
@@ -1,56 +1,15 @@
 import SwiftUI
 
 struct StoriesProgressView: View {
+	// --- internal constants ---
+	let storyPartsCount: Int
+	let progress: CGFloat
 	
-	// MARK: - Private(set) Properties
-    private(set) var currentProgressPhase: StoryAnimationPhase
-	
-	// Whether to enable the fill animation (for current part and only if there is no isCheckedOut)
-	private(set) var isAnimated: Bool
-	
-	// MARK: - Private Constants
-    private let animationTimeout: TimeInterval
-    
-	// MARK: - Private States
-    @State private var displayedValue: CGFloat = 0
-	
-	// MARK: - Internal Init
-	init(
-		currentProgressPhase: StoryAnimationPhase,
-		animationTimeout: TimeInterval,
-		isAnimated: Bool
-	) {
-		self.currentProgressPhase = currentProgressPhase
-		self.animationTimeout = animationTimeout
-		self.isAnimated = isAnimated
-	}
-    
-	// MARK: - Body
+	// --- body ---
     var body: some View {
-        ProgressView(value: displayedValue, total: 1)
-            .progressViewStyle(TravelScheduleStoryProgressViewStyle())
-            .onAppear { syncDisplayedValue() }
-            .onChange(of: currentProgressPhase) {
-                syncDisplayedValue()
-            }
-            .onChange(of: isAnimated) {
-                syncDisplayedValue()
-            }
-    }
-    
-	// MARK: - Private Methods
-    private func syncDisplayedValue() {
-        switch currentProgressPhase {
-        case .start:
-            displayedValue = 0
-        case .end:
-            if isAnimated {
-                withAnimation(.linear(duration: animationTimeout)) {
-                    displayedValue = 1
-                }
-            } else {
-                displayedValue = 1
-            }
-        }
+		ProgressBar(
+			storyPartsCount: storyPartsCount,
+			progress: progress
+		)
     }
 }
