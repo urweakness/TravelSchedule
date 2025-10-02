518b2c2370ff2400789fcef337f327d33f7ff966
timg777 <timg777@yandex.ru>
2025-10-02 18:34:27 +0500

    feat(sprint_20): CGFloat+Extensions add new file with static constant that used in views

--- TravelSchedule/Extensions/CGFloat+Extensions.swift
+++ TravelSchedule/Extensions/CGFloat+Extensions.swift
@@ -1,4 +1,7 @@
+import CoreGraphics
+
 extension CGFloat {
 	static let progressBarCornerRadius: CGFloat = 6
 	static let progressBarHeight: CGFloat = 6
+	static let yDragOffsetAnimationTime: CGFloat = 0.3
 }
