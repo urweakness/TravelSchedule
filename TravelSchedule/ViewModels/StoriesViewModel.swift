189ae1acb02e7ed9bd9874183b340d681b425753
timg777 <timg777@yandex.ru>
2025-10-03 18:59:51 +0500

    refactor(sprint_21): StoriesViewModel FullScreenStories refactor part preparing and managing data for view using TimerManager

--- TravelSchedule/ViewModels/StoriesViewModel.swift
+++ TravelSchedule/ViewModels/StoriesViewModel.swift
@@ -1,55 +1,97 @@
 import SwiftUI
-import Combine
 
 @Observable
 @MainActor
 final class StoriesViewModel {
-	
-	// MARK: - Internal Properties
-	@ObservationIgnored
-    var screenSize: CGSize = .zero
-	
-    @ObservationIgnored
-    var dismiss: DismissAction?
+    // --- manager (source of truth) ---
+	private(set) var manager: StoriesManagerProtocol
 
+    // --- Index for story part ---
+    private(set) var currentStoryPartIndex: Int = 0
+
+    // --- timer ---
+    let timer: TimerManager
+	
+	@ObservationIgnored
+	var dismiss: DismissAction?
+
+	// --- internal init ---
+    init(
+		manager: StoriesManagerProtocol,
+		secondsPerStoryPart: TimeInterval = 5
+	) {
+        self.manager = manager
+        self.currentStoryPartIndex = 0
+        self.timer = TimerManager(secondsPerStoryPart: secondsPerStoryPart)
+        self.timer.delegate = self
+    }
+	
+    // --- proxy accessors ---
+    var stories: [StoryModel] { manager.stories }
+    var currentStoryIndex: Int { manager.currentStoryIndex }
 }
 
 // MARK: - StoriesViewModel Extensions
-
-// MARK: Internal
-
-//MARK: viewDidAppear
-extension StoriesViewModel {
-	func viewDidAppear(
-		dismiss: DismissAction,
-		screenSize: CGSize
-	) {
-		self.dismiss = dismiss
-		self.screenSize = screenSize
+// --- private navigation ---
+private extension StoriesViewModel {
+	func setCurrentStory(index: Int) {
+		guard stories.indices.contains(index) else { return }
+		manager.currentStoryIndex = index
+		currentStoryPartIndex = 0
+		timer.reset()
+	}
+	
+	func performNextStory() {
+		guard stories.indices.contains(currentStoryIndex + 1) else {
+			dismiss?()
+			return
+		}
+		manager.currentStoryIndex += 1
+		currentStoryPartIndex = 0
+		timer.reset()
+	}
+	
+	func performPrevStory() {
+		guard currentStoryIndex > 0 else { return }
+		manager.currentStoryIndex -= 1
+		currentStoryPartIndex = 0
+		timer.reset()
+	}
+	
+	func performNextStoryPart() {
+		setStoryPartCheckedOutStatus()
+		guard let story = stories[safe: currentStoryIndex] else { return }
+		if story.storyParts.indices.contains(currentStoryPartIndex + 1) {
+			currentStoryPartIndex += 1
+			timer.reset()
+		} else {
+			performNextStory()
+		}
+	}
+	
+	func performPrevStoryPart() {
+		if currentStoryPartIndex > 0 {
+			currentStoryPartIndex -= 1
+			timer.reset()
+		} else {
+			performPrevStory()
+		}
 	}
 }
 
-// MARK: viewModel state managing
-extension StoriesViewModel {
-	func performDismissView() {
-		dismiss?()
+// --- private helpers ---
+private extension StoriesViewModel {
+	func setStoryPartCheckedOutStatus() {
+		manager.setStoryPartCheckedOutStatus(
+			currentStoryIndex: currentStoryIndex,
+			currentStoryPartIndex: currentStoryPartIndex
+		)
 	}
 }
 
-// MARK: stories navigation
-extension StoriesViewModel {
-
+// --- TimerViewModelDelegate protocol conforomance ---
+extension StoriesViewModel: @MainActor TimerViewModelDelegate {
+    func timerDidCompletePart() {
+        performNextStoryPart()
+    }
 }
-
-// MARK: - Private
-
-// MARK: Updating isCheckedOut status for concrete part
-private extension StoriesViewModel {
-
-}
-
-// MARK: - Helpers
-private extension StoriesViewModel {
-
-}
-
