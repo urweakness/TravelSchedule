c038b1ebd4e895176b109cda6875b7fcbe069c16
timg777 <timg777@yandex.ru>
2025-10-04 20:33:15 +0500

    refactor(sprint_21): CopyrightService Service is actor now to use restricted execution context to work with network safely Replacing with simplified comments

--- TravelSchedule/Services/CopyrightService.swift
+++ TravelSchedule/Services/CopyrightService.swift
@@ -1,19 +1,22 @@
-final class CopyrightService: CopyrightServiceProtocol {
+final actor CopyrightService: CopyrightServiceProtocol {
 	
-	// MARK: - Private Constants
-    private let client: Client
-    private let apiKey: String
-    
-	// MARK: - Internal Init
-    init(client: Client, apiKey: String) {
-        self.client = client
-        self.apiKey = apiKey
-    }
-    
-	// MARK: - Internal Methods
+	// --- private constants ---
+	private let client: Client
+	private let apiKey: String
+	
+	// --- internal init ---
+	init(client: Client, apiKey: String) {
+		self.client = client
+		self.apiKey = apiKey
+	}
+	
+	// --- internal methods ---
     func getCopyright() async throws -> CopyrightResponse {
         let response = try await client.getCopyright(
-            query: .init(apikey: apiKey, format: .json)
+            query: .init(
+				apikey: apiKey,
+				format: .json
+			)
         )
 
         return try response.ok.body.json
