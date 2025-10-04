b8683ba8470a8e0a4093e7d3cba674ff767e6a8d
timg777 <timg777@yandex.ru>
2025-10-04 20:34:11 +0500

    refactor(sprint_21): StationScheduleService Service is actor now to use restricted execution context to work with network safely Replacing with simplified comments

--- TravelSchedule/Services/StationScheduleService.swift
+++ TravelSchedule/Services/StationScheduleService.swift
@@ -1,18 +1,23 @@
-final class StationScheduleService: StationScheduleServiceProtocol {
+final actor StationScheduleService: StationScheduleServiceProtocol {
 	
-	// MARK: - Private Constants
-    private let client: Client
-    private let apiKey: String
-    
-	// MARK: - Internal Init
-    init(client: Client, apiKey: String) {
-        self.client = client
-        self.apiKey = apiKey
-    }
-    
-	// MARK: - Internal Methods
+	// --- private constants ---
+	private let client: Client
+	private let apiKey: String
+	
+	// --- internal init ---
+	init(client: Client, apiKey: String) {
+		self.client = client
+		self.apiKey = apiKey
+	}
+	
+	// --- internal methods ---
     func getStationSchedule(station: String) async throws -> ScheduleResponse {
-        let response = try await client.getStationSchedule(query: .init(apikey: apiKey, station: station))
+        let response = try await client.getStationSchedule(
+			query: .init(
+				apikey: apiKey,
+				station: station
+			)
+		)
         
         return try response.ok.body.json
     }
