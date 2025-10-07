ad30baf91b794373e5890ececbc621daeb2a0dfb
timg777 <timg777@yandex.ru>
2025-10-07 16:19:57 +0500

    refactor: Town transformed to struct model to support custom towns

--- TravelSchedule/Enums/Town.swift
+++ TravelSchedule/Enums/Town.swift
@@ -1,32 +1,7 @@
-enum Town: TravelPoint {
-    case moscow
-    case saintPetersburg
-    case sochi
-    case gorniy
-    case krasnodar
-    case kazan
-    case omsk
-    
+struct Town: TravelPoint {
     var id: Self { self }
-    
-    var name: String {
-        switch self {
-        case .moscow:
-            return "Москва"
-        case .saintPetersburg:
-            return "Санкт-Петербург"
-        case .sochi:
-            return "Сочи"
-        case .gorniy:
-            return "Горный Воздух"
-        case .krasnodar:
-            return "Краснодар"
-        case .kazan:
-            return "Казань"
-        case .omsk:
-            return "Омск"
-        }
-    }
+	let name: String
+	let code: String
     
     static var noContentTitleText: String {
         "Город не найден"
