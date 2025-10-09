c87551f35dc3ceb0ee8fc21af5eeb2bbd3a2207a
timg777 <timg777@yandex.ru>
2025-10-09 18:58:09 +0500

    test: Unit DataCoordinator tests

--- TravelScheduleTests/DataCoordinatorTests.swift
+++ TravelScheduleTests/DataCoordinatorTests.swift
@@ -3,16 +3,31 @@ import XCTest
 
 final class DataCoordinatorTests: XCTestCase {
 	
-	func testResponse() async {
+	func testStationListServiceResponse() async {
+		let manager = await DataCoordinator()
+		let response = try? await manager.getStationsList()
 		
-		let manager = 
-		let monitor = await
-		
-		XCTAssertFalse(
-			await MainActor.run {
-				monitor.isConnected
-			}
+		XCTAssertNotNil(response?.countries)
+		XCTAssertNotNil(
+			response?.countries?.first(where: { $0.title == "Россия" })
 		)
+	}
+	
+	func testScheduleBetweenStationsServiceResponse() async {
+		let manager = await DataCoordinator()
+		let firstResponse = try? await manager.getScheduleBetweenStations(
+			from: "s9600213",
+			to: "c146"
+		)
+		let secondResponse = try? await manager.getScheduleBetweenStations(
+			from: "XXX",
+			to: "XXX"
+		)
+		
+		XCTAssertNotNil(firstResponse)
+		XCTAssertNil(secondResponse)
+		
+		XCTAssertNotNil(firstResponse?.segments)
 	}
 	
 }
