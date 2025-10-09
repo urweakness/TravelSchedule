59e804c15160ab4cef50c986381246845126c756
timg777 <timg777@yandex.ru>
2025-10-09 18:50:46 +0500

    refactor: TravelTabView receiving view builders as concrete type closures now instead of coordinator dependency accessibilityIdentifier added to use in UI tests it fetches AllStationsResponse now

--- TravelSchedule/Presentation/TravelTabView.swift
+++ TravelSchedule/Presentation/TravelTabView.swift
@@ -2,30 +2,69 @@ import SwiftUI
 
 struct TravelTabView: View {
 	// --- private states ---
+	@State private var viewModel: TravelTabViewModel
     @State private var selectedTab: TravelTab = .main
 	
 	// --- storage ---
     @AppStorage("darkThemeIsActive") private var darkThemeIsActive: Bool = false
-    
+	
 	// --- DI ---
-    let coordinator: Coordinator
-    
+	@Bindable var manager: AppManager
+	let buildMainView: () -> MainView
+	let buildSettingsView: () -> SettingsView
+	
+	init(
+		manager: AppManager,
+		dataCoordinator: DataCoordinator,
+		buildMainView: @escaping () -> MainView,
+		buildSettingsView: @escaping () -> SettingsView
+	) {
+		self.buildMainView = buildMainView
+		self.buildSettingsView = buildSettingsView
+		self.manager = manager
+		_viewModel = .init(
+			initialValue: .init(
+				dataCoordinator: dataCoordinator
+			)
+		)
+	}
+	
 	// --- body ---
     var body: some View {
         TabView(selection: $selectedTab) {
-            coordinator.build(page: .main)
-                .tabItem {
-                    Label("", systemImage: "arrow.up.message.fill")
-                }
-                .tag(TravelTab.main)
+			buildMainView()
+				.tabItem {
+					Label("", systemImage: "arrow.up.message.fill")
+						.accessibilityIdentifier(
+							AccessibilityIdentifier.tabViewMainTabItem.rawValue
+						)
+				}
+				.tag(TravelTab.main)
 			
-            coordinator.build(page: .settings)
-                .tabItem {
-                    Label("", systemImage: "gearshape.fill")
-                }
-                .tag(TravelTab.settings)
+			buildSettingsView()
+				.tabItem {
+					Label("", systemImage: "gearshape.fill")
+						.accessibilityIdentifier(
+							AccessibilityIdentifier.tabViewSettingsTabItem.rawValue
+						)
+				}
+				.tag(TravelTab.settings)
         }
+		.accessibilityIdentifier(
+			AccessibilityIdentifier.tabView.rawValue
+		)
         .tint(Color.travelBlack)
         .preferredColorScheme(darkThemeIsActive ? .dark : .light)
+		.task {
+			await fetchStations()
+		}
     }
+	
+	private func fetchStations() async {
+		guard
+			manager.stationList == nil
+		else { return }
+		manager.stationList = await viewModel
+			.fetchAllStations()
+	}
 }
